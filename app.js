f2d98a5be618ee7f344b515220d4e7fc46bf4639
donljh <don.limjunhong@gmail.com>
2022-05-26 23:07:30 +0800

    Add cors module to allow client and server communication

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 require("./config/database")(); // Connect to DB
+const cors = require('cors');
+
 const routeLogger = require('./middleware/routeLogger');
 
 const express = require("express");
-
 const app = express();  
 
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(routeLogger); // Logs API route hit on any API call
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 
 module.exports = app;
