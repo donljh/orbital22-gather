5b1349046b91536b39a1c1d8b00bf083e024a10f
donljh <don.limjunhong@gmail.com>
2022-06-08 17:30:32 +0800

    Profile page correctly fetches user details

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,8 @@
 import { Box, Card, Divider, Stack, TextField, Typography, IconButton, Button, Modal } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { styled } from '@mui/system';
+import useAxiosRes from '../hooks/useAxiosRes';
 
 const modalStyle = {
   position: 'absolute',
@@ -30,14 +32,16 @@ const profileDisplayStyle = {
 }
 
 const ProfileFieldDisplay = (props) => {
-  return <TextField 
+  return <TextField
             focused 
             color="warning" 
             sx={{ width: '60%' }} 
-            variant={props.variant}  
+            name={props.name}
             label={props.label} 
             value={props.value} 
+            onChange={props.onChange}
             inputProps={{ 
+              type: props.type,
               style: {color: 'white'}, 
               disabled: props.disableInput}}  />
 }
@@ -53,16 +57,45 @@ const EditFieldButton = (props) => {
   )
 }
 
-const FieldChangeForm = (props) => {
+const ProfileFieldChangeModal = (props) => {
+  const { open, onClose, field } = props;
+
+  const axiosRes = useAxiosRes();
+  
+  let path;
+
+  if (field === "name") {
+    path = '/profile/change_name';
+  } else if (field === "email") {
+    path = '/users/change_email';
+  } else if (field === "password") {
+    path = '/users/change_password'
+  }
+
+  const initialFieldData = {
+    previousValue: '',
+    newValue: ''
+  }
+
+  const [fieldData, setFieldData] = useState(initialFieldData);
+
+  const { previousValue, newValue } = fieldData;
+
+  const handleTextFieldChange = e => {
+    setFieldData({ ...fieldData, [e.target.name]: e.target.value })
+    console.log(fieldData)
+  }
 
   return (
-    <Box sx={modalStyle}>
-    <Stack alignItems={'center'} spacing={4} pt={4} >
-      <ProfileFieldDisplay label={`Re-enter your previous ${props.field}`}/>
-      <ProfileFieldDisplay label={`Enter your new ${props.field}`}/>
-      <Button color="success" variant="contained">Save</Button>
-    </Stack>
-  </Box>
+    <Modal open={open} onClose={onClose}>
+      <Box sx={modalStyle}>
+        <Stack alignItems={'center'} spacing={4} pt={4} >
+          <ProfileFieldDisplay type={field} name="previousValue" value={previousValue} onChange={handleTextFieldChange} label={`Enter your previous ${field}`}/>
+          <ProfileFieldDisplay type={field} name="newValue" value={newValue} onChange={handleTextFieldChange} label={`Enter your new ${field}`}/>
+          <Button color="success" variant="contained">Save</Button>
+        </Stack>
+      </Box>
+    </Modal>
   )
 }
 
@@ -70,6 +103,8 @@ const Profile = () => {
   const [nameChangeModalOpen, setNameChangeModalOpen] = useState(false);
   const [emailChangeModalOpen, setEmailChangeModalOpen] = useState(false);
   const [passwordChangeModalOpen, setPasswordChangeModalOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
 
   const openNameChangeModal = () => setNameChangeModalOpen(true);
   const closeNameChangeModal = () => setNameChangeModalOpen(false);
@@ -80,43 +115,45 @@ const Profile = () => {
   const openPasswordChangeModal = () => setPasswordChangeModalOpen(true);
   const closePasswordChangeModal = () => setPasswordChangeModalOpen(false);
 
-  return (
-    <>    
+  const axiosRes = useAxiosRes();
+
+  useEffect(() => {
+    axiosRes.get('/user')
+    .then(response => setEmail(response.data.email))
+    .catch(err => console.log(err));
+    
+    axiosRes
+    .get('/profile')
+    .then(response => setName(response.data.name))
+    .catch(err => console.log(err));
+  })  
+
+  return (  
     <Box sx={profileDisplayStyle}>
       <Stack spacing={8} mb={4}>
         <Typography mt={2} variant="h4" fontWeight="bold">Your Profile</Typography>
 
         <Box>
-          <ProfileFieldDisplay disableInput label="YOUR NAME" value={"name"} />
+          <ProfileFieldDisplay disableInput label="YOUR NAME" value={name} />
           <EditFieldButton onClick={openNameChangeModal}/>
-          <Modal open={nameChangeModalOpen} onClose={closeNameChangeModal}>
-            <FieldChangeForm field='name' />
-          </Modal>
+          <ProfileFieldChangeModal open={nameChangeModalOpen} onClose={closeNameChangeModal} field="name"/>
         </Box>
 
         <Box>
-          <ProfileFieldDisplay disableInput label="YOUR EMAIL ADDRESS"  value={"email123123123123123123123123123131231231"} />
+          <ProfileFieldDisplay disableInput label="YOUR EMAIL ADDRESS"  value={email} />
           <EditFieldButton onClick={openEmailChangeModal}/>
-          <Modal open={emailChangeModalOpen} onClose={closeEmailChangeModal}>
-            <FieldChangeForm field='email' />
-          </Modal>
+          <ProfileFieldChangeModal open={emailChangeModalOpen} onClose={closeEmailChangeModal} field="email"/>
         </Box>
 
         <Box>
           <ProfileFieldDisplay disableInput label="YOUR PASSWORD" value={"********"} />
           <EditFieldButton onClick={openPasswordChangeModal} />
-          <Modal open={passwordChangeModalOpen} onClose={closePasswordChangeModal}>
-            <FieldChangeForm field='password' />
-          </Modal>
+          <ProfileFieldChangeModal open={passwordChangeModalOpen} onClose={closePasswordChangeModal} field="password"/>
         </Box>
 
       </Stack>
       <Button variant="contained" color="error">Deregister</Button>
     </Box>
-
-
-
-    </>
   )
 }
 
