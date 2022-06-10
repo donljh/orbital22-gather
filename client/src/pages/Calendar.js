fab02bc448684b815726c6c3288ee28c294b861e
Han <kynhan123@gmail.com>
2022-06-10 22:25:18 +0800

    Added basic calendar

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <FullCalendar
+        plugins={[ dayGridPlugin, timeGridPlugin ]}
+        initialView="dayGridMonth"
+        events={[
+          { title: 'event 1', date: '2022-06-01' },
+          { title: 'event 2', date: '2022-06-03' }
+        ]}
+    />
   )
 }
 
