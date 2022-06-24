47d77a52f2573465281459670493501c4e116449
donljh <don.limjunhong@gmail.com>
2022-06-24 17:47:21 +0800

    Update README.md and included .env example

--- server/config/database.js
+++ server/config/database.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const { DB_URI_NOADMIN } = process.env;
+const { DB_URI } = process.env;
 
 const connectToDB = () => {
-  // Connect to MongoDB
-mongoose
-  .connect(DB_URI_NOADMIN)
-  .then(() => {
-    console.log("Successfully connected to database");
-  })
-  .catch((error) => {
-    console.log("Failed to connect to database. Exiting now...");
-    console.error(error);
-    process.exit(1);
-  });
-}
+	// Connect to MongoDB
+	mongoose
+		.connect(DB_URI)
+		.then(() => {
+			console.log("Successfully connected to database");
+		})
+		.catch((error) => {
+			console.log("Failed to connect to database. Exiting now...");
+			console.error(error);
+			process.exit(1);
+		});
+};
 
 module.exports = connectToDB;
